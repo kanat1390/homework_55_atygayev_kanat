601fb2f2bc2dea4472b9fb8e1586b093aa770fb7
iprogrammer1390 <iprogrammer1390@gmail.com>
2022-09-28 23:31:56 +0600

    Создана модель Task и зарегистрирована в админ панели

--- src/todo/admin.py
+++ src/todo/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
+from .models import Task
 
-# Register your models here.
+class TaskAdmin(admin.ModelAdmin):
+    exclude = ('deleted_at', 'is_deleted')
+
+
+admin.site.register(Task, TaskAdmin)
+
+
--- src/todo/models.py
+++ src/todo/models.py
@@ -1,3 +1,18 @@
 from django.db import models
 
-# Create your models here.
+STATUS_CHOICES = [
+    ('N', 'Новая'),
+    ('C', 'Выполнена')
+]
+
+class Task(models.Model):
+    title = models.CharField(null=False, blank=False, max_length=40)
+    description = models.TextField(null=True, blank=True, max_length=500)
+    status = models.CharField(choices=STATUS_CHOICES, max_length=1, null=False, blank=False)
+    date_due = models.DateField(null=True, blank=True, default=None)
+    deleted_at = models.DateTimeField(null=True, blank=True, default=None)
+    is_deleted = models.BooleanField(default=False)
+
+    def __str__(self):
+        return self.title
+
